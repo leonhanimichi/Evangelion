86ab15a81b7adcad0da7a0334974f4ab4a155cad
leonhanamichi <leon.lixingyu@gmail.com>
2017-08-18 05:09:46 -0700

    adding setup file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install git
+sudo apt-get install vim-gnome
+sudo apt-get install mutt
+sudo apt-get install python-pip
+pip install -U pip setuptools
+gvim basicStockTrader.py 
+sudo pip install pandas
+sudo pip install pandas_datareader
+sudo apt-get install python-numpy python-scipy python-matplotlib
