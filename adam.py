176695ac57164cfef9879aa6b594f38d7bc33e6f
leonhanamichi <leon.lixingyu@gmail.com>
2017-08-18 04:44:39 -0700

    initial commit

---
+++ adam.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+'''
+Created on Aug 15, 2017
+
+@author: Leon (Jobs), Junaid (Woz)
+'''
+import pandas as pd
+import numpy as np
+import random as rrand
+import math as math
+from pandas_datareader import data as dreader
+
+from pylab import * #Used to calculate Mean and maybe more stuff didn't check, can probably replace/get rid of it
+
+stocks = "TSLA"
+
+#Get data for stocks, from google, starting at 2012-01-15 ending at 2016-09-01
+
+testdata = dreader.DataReader(stocks,'google','2008-01-01','2017-01-01')
+testdata.to_csv('data/TSLA.csv')
+#Use this to print general structure of the data
+#print "DATA: ", testdata
+
+exit()
+#Have to swapaxes because my Trader class uses it like data[stockname]['Close']
+testdata = testdata.swapaxes(0,2)
+
