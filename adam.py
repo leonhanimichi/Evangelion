fe4d6a0ce5c913f8ccfec7bce1bee97044bc6e81
leonhanamichi <leon.lixingyu@gmail.com>
2017-08-19 00:43:48 -0700

    adding ways to read columns of data

--- adam.py
+++ adam.py
@@ -13,13 +13,16 @@ from pandas_datareader import data as dreader
 
 from pylab import * #Used to calculate Mean and maybe more stuff didn't check, can probably replace/get rid of it
 
-stocks = "TSLA"
+stocks = "AAPL"
 
 #Get data for stocks, from google, starting at 2012-01-15 ending at 2016-09-01
 
-testdata = dreader.DataReader(stocks,'google','2008-01-01','2017-01-01')
-testdata.to_csv('data/TSLA.csv')
+allData_temp = dreader.DataReader(stocks,'google','2008-01-01','2009-01-03')
+allData_temp.to_csv('data/'+str(stocks)+'.csv')
+
+allData = pd.read_csv('data/'+str(stocks)+'.csv')
+print allData.Date
 
 exit()
-testdata = testdata.swapaxes(0,2)
+allData = allData.swapaxes(0,2)
 
