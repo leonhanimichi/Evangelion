f68f063bd4615a3511f3b24342174c3205a03152
leonhanamichi <leon.lixingyu@gmail.com>
2017-08-19 00:53:30 -0700

    let everything start from year 2000

--- adam.py
+++ adam.py
@@ -13,11 +13,11 @@ from pandas_datareader import data as dreader
 
 from pylab import * #Used to calculate Mean and maybe more stuff didn't check, can probably replace/get rid of it
 
-stocks = "AAPL"
+stocks = "INTC"
 
 #Get data for stocks, from google, starting at 2012-01-15 ending at 2016-09-01
 
-allData_temp = dreader.DataReader(stocks,'google','2008-01-01','2009-01-03')
+allData_temp = dreader.DataReader(stocks,'google','1970-01-01','2016-08-01')
 allData_temp.to_csv('data/'+str(stocks)+'.csv')
 
 allData = pd.read_csv('data/'+str(stocks)+'.csv')
